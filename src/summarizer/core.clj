95ba309463986503859d82da52fc887963bade06
Alireza Ghasemi <ghasemi.a.ir@gmail.com>
2015-06-03 20:21:37 +0200

    Parallel map/reduce added

--- src/summarizer/core.clj
+++ src/summarizer/core.clj
@@ -5,41 +5,47 @@
     )
     (:require
       [clojure.math.numeric-tower :as math]
+      [clojure.core.reducers :as r]
       )
 )
 
+;Define tokenizer and sentence detector from OpenNLP
 (def get-sentences (make-sentence-detector "models/en-sent.bin"))
 (def tokenize (make-tokenizer "models/en-token.bin"))
 
-;Number of repetitions of item x in collection coll
+;Returns the number of repetitions of item x in collection coll
 (defn number-of-repetitions-of [x coll] (
                                            count (filter (fn [y] (= x y)) coll)
                                            )
   )
 
 ;Returns a hash-map mapping each unique item in coll to its number of appearances in coll
-(defn count-repetitions-of-each-item-in-collection [coll] (
-                                 into {} ;To convert to hash-map
-                                 (
-                                   let [coll-as-set (set coll)]
-                                 (
-                                   map (fn [x] [ x   (number-of-repetitions-of x coll)])
-                                   coll-as-set
-                                   )
-                                 )
-                                 )
+(defn count-repetitions-of-each-item-in-collection [coll]
+  (
+    into {} ;To convert to hash-map
+    (
+      let [coll-as-set (set coll)]
+      (
+         r/map (fn [x] [ x   (number-of-repetitions-of x coll)])
+         coll-as-set
+        )
+      )
+
+    )
+
   )
 
 ;Computes the score of a sentences based on how frequent its constituent words are in the whole text.
 (defn sentence-score [sentence word-scores]
   (let [sentence-tokens (tokenize sentence)]
-    (reduce + (map
-                (fn [x] (
-                          let [score (get word-scores x)]
-                          (if (nil? score) 0 score)
-                          ))
-                sentence-tokens
-                )
+    (reduce + (r/map
+        (fn [x] (
+                  let [score (get word-scores x)]
+                  (if (nil? score) 0 score)
+                  )
+          )
+        sentence-tokens
+        )
       )
 
     )
@@ -49,7 +55,7 @@
 (defn compute-sentence-scores [text word-scores]
   (let [scores (count-repetitions-of-each-item-in-collection (tokenize text))]
     (let [sentences (get-sentences text)]
-     (map
+     (r/map
        (fn [sentence] (sentence-score sentence scores))
        sentences
 
